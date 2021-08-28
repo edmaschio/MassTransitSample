e5ad1dfca96ce597158b17b9b40e4e99a3a8e800
Ederson Tadeu Maschio <etmaschio@smarapd.com.br>
2021-08-28 17:21:34 -0300

    create masstransit extension

---
+++ Extensions/MassTransitExtension.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using MassTransit;
+
+namespace MassTransitSample.Extensions
+{
+    /// <summary>
+    /// MassTransitExtension
+    /// </summary>
+    public static class MassTransitExtension
+    {
+        /// <summary>
+        /// AddRabbitMQ configs
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public static void AddRabbitMQ(this IServiceCollection services, IConfiguration configuration)
+        {
+            var massTransitSection = configuration.GetSection("MassTransit");
+            var url = massTransitSection.GetValue<string>("Uri");
+            var userName = massTransitSection.GetValue<string>("Username");
+            var password = massTransitSection.GetValue<string>("Password");
+
+            if (massTransitSection == null || string.IsNullOrEmpty(url) || string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("Section 'mass-transit' configuration settings are not found in appsettings.json");
+            }
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<MessageConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host
+                    (
+                        new Uri(url),
+                        hostdata =>
+                        {
+                            hostdata.Username(userName);
+                            hostdata.Password(password);
+                        }
+                    );
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            services.AddMassTransitHostedService(true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 using System;
+using MassTransitSample.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MassTransit;
 
 namespace MassTransitSample
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -16,30 +16,7 @@ namespace MassTransitSample
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddMassTransit(x =>
-                    {
-                        x.AddConsumer<MessageConsumer>();
-
-                        //x.UsingInMemory((context, cfg) => cfg.ConfigureEndpoints(context));
-
-                        x.UsingRabbitMq((context, cfg) =>
-                        {
-                            cfg.Host
-                            (
-                                new Uri(hostContext.Configuration["RabbitMQ:Uri"]),
-                                hostdata =>
-                                {
-                                    hostdata.Username(hostContext.Configuration["RabbitMQ:Username"]);
-                                    hostdata.Password(hostContext.Configuration["RabbitMQ:Password"]);
-                                }
-                            );
-
-                            cfg.ConfigureEndpoints(context);
-                        });
-                    });
-
-                    services.AddMassTransitHostedService(true);
-
+                    services.AddRabbitMQ(hostContext.Configuration);
                     services.AddHostedService<Worker>();
                 });
     }
